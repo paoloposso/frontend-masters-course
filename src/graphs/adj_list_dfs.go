b7940dfa863a354da0210af292bb54224c7a2380
Paolo Posso <pvictorsys@gmail.com>
2024-03-24 16:04:10 -0300

    adj list dfs

--- src/graphs/adj_list_dfs.go
+++ src/graphs/adj_list_dfs.go
@@ -1,14 +1,61 @@
 package graphs
 
-type AdjancencyList map[int][]int
+type AdjacencyList map[int][]edge
 
-func NewGraphAdjList() AdjancencyList {
-	return map[int][]int{}
+type edge struct {
+	to, weight int
 }
 
-func (a AdjancencyList) addEdge(from, to int) {
-	if _, ok := a[from]; !ok {
-		a[from] = []int{}
+func NewGraphAdjList() AdjacencyList {
+	return make(map[int][]edge)
+}
+
+func (gr AdjacencyList) addEdge(from, to int) {
+	if _, ok := gr[from]; !ok {
+		gr[from] = []edge{}
 	}
-	a[from] = append(a[from], to)
+	gr[from] = append(gr[from], edge{to: to, weight: 1})
+}
+
+func (gr AdjacencyList) dfs(source, needle int) (bool, []int) {
+	seen := map[int]bool{}
+
+	for i := range gr {
+		seen[i] = false
+	}
+
+	path := &[]int{}
+
+	itemFound := gr.dfsRecur(source, needle, seen, path)
+
+	if itemFound {
+		return true, append(*path, needle)
+	}
+	return false, []int{}
+}
+
+func (gr AdjacencyList) dfsRecur(curr, needle int, seen map[int]bool, path *[]int) bool {
+	if curr == needle {
+		return true
+	}
+	if seen[curr] {
+		return false
+	}
+
+	seen[curr] = true
+
+	*path = append(*path, curr)
+
+	adj := gr[curr]
+
+	for _, edge := range adj {
+		if gr.dfsRecur(edge.to, needle, seen, path) {
+			return true
+		}
+	}
+
+	// pop
+	*path = (*path)[:len(*path)-1]
+
+	return false
 }
--- src/graphs/adj_list_dfs_test.go
+++ src/graphs/adj_list_dfs_test.go
@@ -1,6 +1,8 @@
 package graphs
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCreateNodeAdjList(t *testing.T) {
 	g := NewGraphAdjList()
@@ -10,4 +12,44 @@ func TestCreateNodeAdjList(t *testing.T) {
 	g.addEdge(2, 3)
 	g.addEdge(3, 4)
 	g.addEdge(4, 2)
+
+	if len(g[1]) != 2 {
+		t.Fail()
+	}
+}
+
+func TestDfs(t *testing.T) {
+	g := NewGraphAdjList()
+
+	g.addEdge(1, 2)
+	g.addEdge(2, 4)
+	g.addEdge(2, 5)
+	g.addEdge(4, 5)
+	g.addEdge(4, 6)
+	g.addEdge(6, 3)
+
+	found, path := g.dfs(1, 3)
+
+	if found == false {
+		t.Fail()
+	}
+	if path[len(path)-1] != 3 {
+		t.Fail()
+	}
+	if path[0] != 1 {
+		t.Fail()
+	}
+}
+
+func TestDfsEmpty(t *testing.T) {
+	g := NewGraphAdjList()
+
+	found, path := g.dfs(1, 3)
+
+	if found == true {
+		t.Fail()
+	}
+	if len(path) > 0 {
+		t.Fail()
+	}
 }
