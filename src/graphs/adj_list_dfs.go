3b4ea5ec83495f96fc9fef6e8f3bb0df43ab1a7e
Paolo Posso <pvictorsys@gmail.com>
2024-04-03 18:22:58 -0300

    Solve Dijkstra

--- src/graphs/adj_list_dfs.go
+++ src/graphs/adj_list_dfs.go
@@ -6,18 +6,28 @@ type edge struct {
 	to, weight int
 }
 
+const infinity int = 999999999999999999
+
 func NewGraphAdjList() AdjacencyList {
 	return make(map[int][]edge)
 }
 
-func (gr AdjacencyList) addEdge(from, to int) {
+func (gr AdjacencyList) AddEdge(from, to, weight int) {
 	if _, ok := gr[from]; !ok {
 		gr[from] = []edge{}
 	}
-	gr[from] = append(gr[from], edge{to: to, weight: 1})
+	gr[from] = append(gr[from], edge{to: to, weight: weight})
+
+	if _, ok := gr[to]; !ok {
+		for c := len(gr) - 1; c <= to; c++ {
+			if _, ok := gr[c]; !ok {
+				gr[c] = []edge{}
+			}
+		}
+	}
 }
 
-func (gr AdjacencyList) dfs(source, needle int) (bool, []int) {
+func (gr AdjacencyList) Dfs(source, needle int) (bool, []int) {
 	seen := map[int]bool{}
 
 	for i := range gr {
@@ -58,4 +68,88 @@ func (gr AdjacencyList) dfsRecur(curr, needle int, seen map[int]bool, path *[]in
 	*path = (*path)[:len(*path)-1]
 
 	return false
+}
+
+func (graph AdjacencyList) Dijkstra(source, sink int) []int {
+	seen := make([]bool, len(graph))
+
+	for i := range seen {
+		seen[i] = false
+	}
+
+	dists := make([]int, len(graph))
+
+	for i := range dists {
+		dists[i] = infinity
+	}
+
+	prev := make([]int, len(graph))
+
+	for i := range prev {
+		prev[i] = -1
+	}
+
+	dists[source] = 0
+
+	for hasUnvisited(seen, dists) {
+		curr := getLowestUnvisited(seen, dists)
+		seen[curr] = true
+
+		adjs := graph[curr]
+
+		for _, edge := range adjs {
+			if seen[edge.to] {
+				continue
+			}
+
+			dist := dists[curr] + edge.weight
+			if dist < dists[edge.to] {
+				dists[edge.to] = dist
+				prev[edge.to] = curr
+			}
+		}
+	}
+
+	curr := sink
+	path := []int{}
+
+	for prev[curr] != -1 {
+		path = append(path, curr)
+		curr = prev[curr]
+	}
+
+	result := []int{}
+
+	for i := range path {
+		result = append(result, path[len(path)-1-i])
+	}
+
+	return append([]int{source}, result...)
+}
+
+func hasUnvisited(seen []bool, dists []int) bool {
+	for i, s := range seen {
+		if !s && dists[i] < infinity {
+			return true
+		}
+	}
+	return false
+}
+
+func getLowestUnvisited(seen []bool, dists []int) int {
+	result := -1
+	lowestDist := infinity
+
+	for i, s := range seen {
+		if s {
+			continue
+		}
+
+		if lowestDist > dists[i] {
+			lowestDist = dists[i]
+			result = i
+		}
+	}
+
+	return result
 }
--- src/graphs/adj_list_dfs_test.go
+++ src/graphs/adj_list_dfs_test.go
@@ -1,17 +1,18 @@
 package graphs
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCreateNodeAdjList(t *testing.T) {
 	g := NewGraphAdjList()
 
-	g.addEdge(1, 2)
-	g.addEdge(1, 3)
-	g.addEdge(2, 3)
-	g.addEdge(3, 4)
-	g.addEdge(4, 2)
+	g.AddEdge(1, 2, 1)
+	g.AddEdge(1, 3, 1)
+	g.AddEdge(2, 3, 1)
+	g.AddEdge(3, 4, 1)
+	g.AddEdge(4, 2, 1)
 
 	if len(g[1]) != 2 {
 		t.Fail()
@@ -21,14 +22,14 @@ func TestCreateNodeAdjList(t *testing.T) {
 func TestDfs(t *testing.T) {
 	g := NewGraphAdjList()
 
-	g.addEdge(1, 2)
-	g.addEdge(2, 4)
-	g.addEdge(2, 5)
-	g.addEdge(4, 5)
-	g.addEdge(4, 6)
-	g.addEdge(6, 3)
+	g.AddEdge(1, 2, 1)
+	g.AddEdge(2, 4, 1)
+	g.AddEdge(2, 5, 1)
+	g.AddEdge(4, 5, 1)
+	g.AddEdge(4, 6, 1)
+	g.AddEdge(6, 3, 1)
 
-	found, path := g.dfs(1, 3)
+	found, path := g.Dfs(1, 3)
 
 	if found == false {
 		t.Fail()
@@ -44,7 +45,7 @@ func TestDfs(t *testing.T) {
 func TestDfsEmpty(t *testing.T) {
 	g := NewGraphAdjList()
 
-	found, path := g.dfs(1, 3)
+	found, path := g.Dfs(1, 3)
 
 	if found == true {
 		t.Fail()
@@ -57,19 +58,36 @@ func TestDfsEmpty(t *testing.T) {
 func TestNonExistingValue(t *testing.T) {
 	g := NewGraphAdjList()
 
-	g.addEdge(1, 2)
-	g.addEdge(2, 4)
-	g.addEdge(2, 5)
-	g.addEdge(4, 5)
-	g.addEdge(4, 6)
-	g.addEdge(6, 3)
+	g.AddEdge(1, 2, 1)
+	g.AddEdge(2, 4, 1)
+	g.AddEdge(2, 5, 1)
+	g.AddEdge(4, 5, 1)
+	g.AddEdge(4, 6, 1)
+	g.AddEdge(6, 3, 1)
 
-	found, path := g.dfs(1, 7)
+	found, path := g.Dfs(1, 7)
 
 	if found == true {
 		t.FailNow()
 	}
 	if len(path) > 0 {
 		t.FailNow()
+	}
+}
+
+func TestDijkstra(t *testing.T) {
+	g := NewGraphAdjList()
+
+	g.AddEdge(0, 1, 8)
+	g.AddEdge(0, 2, 2)
+	g.AddEdge(1, 2, 1)
+	g.AddEdge(2, 4, 12)
+	g.AddEdge(2, 3, 2)
+	g.AddEdge(3, 4, 2)
+
+	result := g.Dijkstra(0, 4)
+
+	if fmt.Sprint(result) != "[0 2 3 4]" {
+		t.Fail()
 	}
 }
